255f75e8358a5e4b7ec2685ef5a7592bf7369b0e
Felipe Hidequel <felipehidequel@gmail.com>
2022-12-11 16:03:05 -0300

    first commit

---
+++ lista.py
@@ -0,0 +1,12 @@
+lista_convidados = []
+
+x = int(input("Entre com o numero de convidados: "))
+for i in range(1, x+1):
+    nome = input(f"Insira com o nome do convidado {i}: ")
+    lista_convidados.append(nome)
+remove_nome_repetido = set(lista_convidados)
+lista_final = tuple(remove_nome_repetido)
+
+print("A lista de convidados é: ")
+for i in range(len(lista_final)):
+    print(lista_final[i])
