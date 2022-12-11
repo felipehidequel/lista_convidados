2acbc5cb3a63e58c3a152279e39f16b434650a77
brunopaiva1 <brunopaivamaster10@gmail.com>
2022-12-11 18:31:50 -0300

    second commit

--- lista.py
+++ lista.py
@@ -1,10 +1,15 @@
-#from PySimpleGUI import PySilmpleGUI as sg
+from PySimpleGUI import PySilmpleGUI as sg
 
 
 lista_convidados = []
 
+
+layout = [
+    [sg.Text('Insira o número de convidados'), sg.Input(key='convidado')]
+]
 x = int(input("Entre com o numero de convidados: "))
 for i in range(1, x+1):
+
     nome = input(f"Insira o nome do convidado {i}: ")
     lista_convidados.append(nome)
 remove_nome_repetido = set(lista_convidados)
