0a94d012d4868ef6ffe51045405f43989f2b3640
brunopaiva1 <brunopaivamaster10@gmail.com>
2022-12-11 18:39:31 -0300

    three commit

--- lista.py
+++ lista.py
@@ -6,15 +6,23 @@ lista_convidados = []
 
 layout = [
     [sg.Text('Insira o número de convidados'), sg.Input(key='convidado')]
+    #for i in range(1, convidado+1):
 ]
-x = int(input("Entre com o numero de convidados: "))
-for i in range(1, x+1):
+# #x = int(input("Entre com o numero de convidados: "))
+# for i in range(1, convidado+1):
 
-    nome = input(f"Insira o nome do convidado {i}: ")
-    lista_convidados.append(nome)
-remove_nome_repetido = set(lista_convidados)
-lista_final = tuple(remove_nome_repetido)
+#     nome = input(f"Insira o nome do convidado {i}: ")
+#     lista_convidados.append(nome)
+# remove_nome_repetido = set(lista_convidados)
+# lista_final = tuple(remove_nome_repetido)
 
-print("A lista de convidados é: ")
-for i in range(len(lista_final)):
-    print(lista_final[i])
+# print("A lista de convidados é: ")
+# for i in range(len(lista_final)):
+#     print(lista_final[i])
+
+janela = sg.Window('Menu', layout) # layout que define o nome na janela
+
+while True:
+    acao, valor = janela.read()
+    if acao == sg.WINDOW_CLOSED: #fechamento da janela caso o usuario não queira insirir umvalor
+        break
