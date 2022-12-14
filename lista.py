7956ee5cf540cfe3bc2a1a9ade916c902749e8c4
brunopaiva1 <brunopaivamaster10@gmail.com>
2022-12-14 18:50:39 -0300

    .

--- lista.py
+++ lista.py
@@ -1,7 +1,7 @@
 from PySimpleGUI import PySimpleGUI as sg
 
 sg.theme('DarkPuple4')
-lista_convidados = []
+
 
 layout = [
     [sg.Text('Insira o número de convidados'), sg.Input(key='convidado', size=(10, 3))], #layout
@@ -17,8 +17,5 @@ while True:
         break
     if acao == 'Confirmar':
         print("----- LISTA DE CONVIDADOS -----")
-        for i in range(1, int(numero)+1):       
-            remove_nome_repetido = set(lista_convidados)
-            lista_final = tuple(remove_nome_repetido)
-            for i in range(len(lista_final)):
-                print(lista_final[i])
+        for i in range(1, int(numero)+1):
+            nomes = input('nome do convidado: ')
